d49039c6db09677d071fd072f871972c747e7cc8
Haky86 <hakamelassouad@gmail.com>
2023-01-25 18:56:56 +0100

    a70q: Fix common trees inclusion
    Change-Id: Id009d357937650cd2af0ab31e13a7d371a92e9e7

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,7 +14,7 @@
 # limitations under the License.
 #
 
-include device/samsung/sm6150-common/BoardConfigCommon.mk
+-include device/samsung/sm6150-common/BoardConfigCommon.mk
 
 DEVICE_PATH := device/samsung/a70q
 
